1ab740fdace2ebdc6275e27d6efc4024bddb9335
Vivi440 <80990541+Vivi440@users.noreply.github.com>
2022-09-12 17:44:53 -0300

    Actualizacion de Scripts

---
+++ Bang-Boom-Wham-main/Project Cult/Assets/Enemies/Scripts/EnemyDamage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public Health Health;
+    public int damage = 2;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Health.TakeDamage(1);
+        }
+    }
+}
