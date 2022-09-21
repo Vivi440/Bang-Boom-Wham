64bbb990b83a7df1f71d5854d4806a920b486f13
Vivi440 <80990541+Vivi440@users.noreply.github.com>
2022-09-20 23:58:35 -0400

    HEEAYVOIDABLVDABLVADHNBÑKVBADN

--- Bang-Boom-Wham-main/Project Cult/Assets/Player/Scripts/CameraFollow.cs
+++ Bang-Boom-Wham-main/Project Cult/Assets/Player/Scripts/CameraFollow.cs
@@ -1,18 +1,57 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField]
+    GameObject player;
 
-    public float followSpeed = 2f;
-    public float yoffset = 1f;
-    public Transform target;
-    
+    [SerializeField]
+    float timeOffset;
+
+    [SerializeField]
+    Vector2 posOffset;
+
+    [SerializeField]
+    float leftLimit;
+    [SerializeField]
+    float rightLimit;
+    [SerializeField]
+    float bottomLimit;
+    [SerializeField]
+    float topLimit;
+
+    private Vector3 velocity;
+
     void Update()
     {
-       Vector3 newPos = new Vector3(target.position.x, target.position.y +yoffset, -10f);
-        transform.position = Vector3.Slerp(transform.position,newPos,followSpeed * Time.deltaTime);
+        Vector3 startPos = transform.position;
 
+        Vector3 endPos = player.transform.position;
+
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z += -10;
+
+        transform.position = Vector3.SmoothDamp(startPos, endPos, ref velocity, timeOffset);
+
+
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z
+        );
     }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
+    }
+
 }
